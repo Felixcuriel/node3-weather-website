b1709ebf4c336b46dc1845a24d33d580c54c897f
Felix Curiel <felicuriel15@hotmail.com>
2021-01-05 22:24:46 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,9 @@
-//console.log('Client side javascript file is loaded!')
-/*
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
- response.json().then((data)=>{
-console.log(data)
- })   
-})
-*/
-/*
-fetch('http://localhost:3000/weather?address=Madrid').then((response)=>{
- response.json().then((data)=>{
-     if(data.error){
-         console.log(data.error)
-     }else{
-console.log(data.location)
-console.log(data.forecast)
-     }
- })   
-})
-*/
+
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-
-//messageOne.textContent = 'From JavaScript'
 
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault()
@@ -38,10 +17,8 @@ weatherForm.addEventListener('submit', (e)=>{
      }else{
 messageOne.textContent = data.location
 messageTwo.textContent = data.forecast
-  //      console.log(data.location)
-//console.log(data.forecast)
+  
      }
  })   
 })
-//console.log(location)
 })
